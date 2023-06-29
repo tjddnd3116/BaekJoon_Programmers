a2ed8a1d8ca72c2cc5d8c8bf537c993fca669dc2
tjddnd3116 <tjddnd3116@gmail.com>
2023-06-29 17:42:25 +0900

    [ADD] Readme.md 추가
    - Readme.md 추가

--- string/459_RepeatedSubstringPattern/main.cpp
+++ string/459_RepeatedSubstringPattern/main.cpp
@@ -1,27 +1,6 @@
 #include <iostream>
 #include <string>
 
-// Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
-//
-// Example 1:
-//
-// Input: s = "abab"
-// Output: true
-// Explanation: It is the substring "ab" twice.
-// Example 2:
-//
-// Input: s = "aba"
-// Output: false
-// Example 3:
-//
-// Input: s = "abcabcabcabc"
-// Output: true
-// Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
-
-// solution
-//
-// 주어진 s를 합하여 제일앞, 제일 마지막의 단어를 제외한 단어에서 원래의 s가 있는지 확인
-// 없으면 false, 있다면 true 리턴
 
 using namespace std;
 
--- two_pointers/283_MoveZeroes/main.cpp
+++ two_pointers/283_MoveZeroes/main.cpp
@@ -4,38 +4,6 @@
 
 using namespace std;
 
-// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-//
-// NOTE that you must do this in-place without making a copy of the array.
-//
-// Example 1:
-// Input: nums = [0,1,0,3,12]
-// Output: [1,3,12,0,0]
-//
-// Example 2:
-// Input: nums = [0]
-// Output: [0]
-//
-// Constraints:
-// 1 <= nums.length <= 104
-// -231 <= nums[i] <= 231 - 1
-//
-// TOPICS:
-// Array, Two Pointers
-
-// solution
-// 추가적인 배열을 사용하지 못함
-// 한번의 순회로 정렬하는 방법을 찾아야함
-// 순회를 하면서 0이면 해당 원소를 삭제하고 push_back으로 0을 추가
-// 0이 아니면 배열순회
-// -> 0, 0, 1 일때 정상적으로 동작하지 않음
-//
-// two pointer를 사용해서 풀어야함
-// left는 제일 왼쪽에 있는 0을 가르킴
-// right가 순회하면서 left보다 오른쪽에 있으면서 0이 아니면 left와의 원소를 바꿈
-// left는 그 다음의 제일 왼쪽에 있는 0을 가르킴
-// 반복
-
 void moveZeroes(vector<int>& nums)
 {
 	for (auto left = nums.begin(), right = nums.begin(); right != nums.end(); right++)
